7bcda06e77181c861dedb050b463aa571f01d308
Tom Nguyen <tndallas9517@gmail.com>
2022-10-24 16:49:07 -0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ContactMVC.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Models/AppUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContactMVC.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage="The {0} must be at least {2} and max {1} characters long.", MinimumLength = 2)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage="The {0} must be at least {2} and max {1} characters long.", MinimumLength = 2)]
+        public string? LastName { get; set; }
+
+        [NotMapped]
+        public string? FullName { get { return $"{FirstName} {LastName}"; } }
+
+        // TODO: make relationship to Contact model
+
+        // TODO: make relationship to Category model
+    }
+}
